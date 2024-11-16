7f1fbeee849b43a69fa52210a4dd7fcb7e3be564
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-16 19:38:41 +0100

    commit1

---
+++ language-model.py
@@ -0,0 +1,50 @@
+from encodings.punycode import generate_generalized_integer
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import random
+
+anvandningar_lista = {
+    "Stuff1": ["1Stuff"],
+    "Stuff2": ["2Stuff"]
+}
+
+svars_lista = {
+    "sak1": [
+        "Svarsmöjlighet 1.1"
+        "svarsmöjlighet 1.2"
+    ],
+    "sak2": [
+        "Svarsmöjlighet 2.1"
+        "Svarsmöjlighet 2.2"
+    ]
+}
+#preprocess user input
+def preprocess_input(user_input):
+    tokens = word_tokenize(user_input)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens =[word for word in tokens if word not in stop_words]
+    return filtered_tokens
+
+
+def detection_function(user_input):
+    tokens = preprocess_input(user_input)
+
+    for category, keywords in anvandningar_lista.item():
+        if any(keyword in tokens for keyword in keywords):
+            return category
+    return "general"
+
+def generate_response(issue_category):
+    if issue_category in svars_lista:
+        return random.choice(responses[issue_category])
+    else:
+        return ("Ber om ursäkt, men kan inte hjälpa dig med det.")
+
+#hur functionerna skulle funka
+    """"issue_category = detect_issue(user_input)
+
+    response = generate_response(issue_category)
+
+    print(response)"""
---
+++ ui.py
@@ -0,0 +1,84 @@
+from tkinter import *
+import customtkinter
+
+
+
+count = 0
+
+root = customtkinter.CTk()
+
+root.geometry("800x500")
+
+customtkinter.set_default_color_theme("green")
+
+custom_font = customtkinter.CTkFont(family="Helvetica", size=15, weight="bold")
+
+customtkinter.set_appearance_mode("dark")
+
+def exit():
+    root.destroy()
+
+def send(*args) -> str:
+    user_input = entry.get()
+    clear_text()
+    change_placement(button_rely=0.9, entry_rely=0.9)
+    label.destroy()
+    add_frame(fg_color=None)
+    text_bubble(user_input)
+    if user_input == "sluta":
+        exit()
+    return user_input
+
+
+
+def length(user_input):
+    length = custom_font.measure(text=user_input) + 20
+    return length
+
+def text_bubble(user_input):
+    frame_bubble = add_frame(width=length(user_input), height=30, fg_color="grey", bg_color="#2B2B2B")
+    label_bubble = add_label(master=frame_bubble, text=user_input, anchor=W, relx=0.1, text_color="#141414")
+    label_bubble.pack(padx=10)
+    return label_bubble
+
+
+
+def add_label(master=root, text="Label", anchor=CENTER, relx=0.5, rely=0.5, fg_color="transparent", bg_color="transparent", text_color=None):
+    label_add = customtkinter.CTkLabel(master=master, text=text, font=custom_font, fg_color=fg_color, bg_color=bg_color, text_color=text_color)
+    label_add.place(relx=relx, rely=rely, anchor=anchor)
+    return label_add
+
+def add_frame(master=root, width=700, height=350, corner_radius=15, fg_color="transparent", bg_color="transparent"):
+    frame = customtkinter.CTkFrame(master=master, width=width, height=height, corner_radius=corner_radius, fg_color=fg_color, bg_color=bg_color)
+    frame.place(relx=0.5, rely=0.45, anchor=CENTER)
+    return frame
+
+def clear_text():
+    entry.delete(0, "end")
+
+def change_placement(button_relx: float=0.85,
+                     button_rely: float=0.57,
+                     entry_relx: float=0.39,
+                     entry_rely: float=0.57,
+                     label_relx: float=0.39,
+                     label_rely: float= 0.38,
+                     *args):
+    button_send.place(relx=button_relx, rely=button_rely)
+    entry.place(relx=entry_relx, rely=entry_rely)
+
+
+button_send = customtkinter.CTkButton(master=root, text="Send", command=send)
+
+button_send.place(relx=0.85, rely=0.57, anchor=CENTER)
+
+entry = customtkinter.CTkEntry(master=root, placeholder_text="Skicka ett medalande till mig!", width=520)
+
+entry.place(relx=0.39, rely=0.57, anchor=CENTER)
+
+root.bind("<Return>", send)
+
+label = customtkinter.CTkLabel(master=root, text="Hej, vad kan jag hjälpa till med?", font=("<Arial>", 35))
+
+label.place(relx=0.39, rely=0.38, anchor=CENTER)
+
+root.mainloop()
