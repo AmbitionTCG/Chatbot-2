0d08d1f87258dbe778477dd56a0898db5506baf2
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-18 13:42:13 +0100

    commit1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import ui
+import time
+
+start_time = time.time() + 31
+elapsed_time = time.time() - start_time
+
+class VariableWatcher:
+    def __init__(self):
+        self._value = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
+        self.on_change(new_value)
+
+    def on_change(self, new_value):
+        start_time = time.time()
+
+while timing:
+    if elapsed_time < 30:
+        print("reset func here")
+        break
