e8f7f7c868b6761c622fc725dfcd965f252dbf52
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-20 11:10:29 +0100

    commit1

--- Registration.py
+++ Registration.py
@@ -1,11 +1,7 @@
 import ui
 
 RegVar = False
-currentUserInput = ""
 
 def RegFunc():
     print("regfunc here")
     RegVar = True
-    currentUserInput = ui.user_input
-    if ui.user_input != currentUserInput:
-        print("Edit into excel code here")
