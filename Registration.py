2ff70fa0b163d37a2f9df75902ae9416db82486a
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-22 09:31:50 +0100

    commit2

--- Registration.py
+++ Registration.py
@@ -11,4 +11,4 @@ def RegFunc():
         with open('User_inputs.txt', 'w+') as f:
             f.write('%s\n' %ui.user_input)
             print("file succesfully written in!")
-            f.close()
+        f.close()
