51106d511b3fd4a30dea7de003f387181e5327a5
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-22 13:44:44 +0100

    commit2

--- Registration.py
+++ Registration.py
@@ -1,14 +1,11 @@
 import ui
 
 RegVar = False
-currentuserinput = ""
 
 def RegFunc():
     print("regfunc here")
     RegVar = True
-    currentuserinput = ui.user_input
-    if currentuserinput != ui.user_input:
-        with open('User_inputs.txt', 'w+') as f:
+    with open('User_inputs.txt', 'w+') as f:
             f.write('%s\n' %ui.user_input)
             print("file succesfully written in!")
-        f.close()
+    f.close()
