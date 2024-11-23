8f0ba3b176360d9a70fedf84e5bdcb9156db8b4e
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-11-23 17:36:44 +0100

    commit2

--- Registration.py
+++ Registration.py
@@ -6,7 +6,7 @@ def RegFunc(write="erm, what the sigma"):
     print("regfunc here")
     RegVar = True
     with open('User_inputs.txt', 'w+') as f:
-            f.write(write)
+            f.write('\n' + write)
             print("file succesfully written in!")
 
 
