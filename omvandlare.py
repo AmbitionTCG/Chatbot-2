b2e43ce36d59b90ed1d80b78b7b9dd63c94aeedb
Creatorofchatbots <inspelningsprogram+githubchatbot@gmail.com>
2024-12-03 15:12:24 +0100

    Update omvandlare.py

--- omvandlare.py
+++ omvandlare.py
@@ -1,3 +1,20 @@
-def omvandlare(tokens):
-    match tokens:
-        case "celsius":
+def hitta_enheter(tokens):
+    
+    enheter = [
+    "Centimeter", "cm", "Meter", "m", "Kilometer", "km", "Millimeter", "mm", 
+    "Tum", "in", "Fot", "ft", "Yard", "yd", "Mile", "mi",
+    
+    "Gram", "g", "Kilogram", "kg", "Ton", "t", "Milligram", "mg", 
+    "Ounce", "oz", "Pound", "lb", "Stone", "st",
+    
+    "Liter", "l", "Milliliter", "ml", "Kubikmeter", "m³", 
+    "Kubikcentimeter", "cm³", "Cubic inch", "in³", "Gallon", "gal", 
+    "Quart", "qt", "Pint", "pt", "Cup", "cup", ,
+    
+    "Celsius", "°C", "Fahrenheit", "°F", "Kelvin", "K", "Rankine", "°R"
+]
+    output = []
+    for i in tokens:
+        if i in enheter:
+            output.append(i)
+    return output
