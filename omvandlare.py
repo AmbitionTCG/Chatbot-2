16e412919f6e69e9871180b8fa1f22fc451e57c0
Richard Mogyorosi <hungarianbearproductions@gmail.com>
2024-12-04 09:51:23 +0100

    commit2

--- omvandlare.py
+++ omvandlare.py
@@ -1,17 +1,17 @@
 def hitta_enheter(tokens):
     
     enheter = [
-    "Centimeter", "cm", "Meter", "m", "Kilometer", "km", "Millimeter", "mm", 
-    "Tum", "in", "Fot", "ft", "Yard", "yd", "Mile", "mi",
+    "Centimeter", "centimeter", "cm", "Meter", "meter", "m", "kilometer", "Kilometer", "km", "Millimeter", "millimeter", "mm",
+    "Tum", "tum", "in", "Fot", "fot", "ft", "Yard", "yard", "yd", "Mile", "mile", "mi",
     
-    "Gram", "g", "Kilogram", "kg", "Ton", "t", "Milligram", "mg", 
-    "Ounce", "oz", "Pound", "lb", "Stone", "st",
+    "Gram", "gram", "g", "Kilogram", "kilogram", "kg", "Ton", "ton", "t", "Milligram", "milligram", "mg",
+    "Ounce", "ounce", "oz", "Pound", "pound", "lb", "Stone", "stone", "st",
     
-    "Liter", "l", "Milliliter", "ml", "Kubikmeter", "m³", 
-    "Kubikcentimeter", "cm³", "Cubic inch", "in³", "Gallon", "gal", 
-    "Quart", "qt", "Pint", "pt", "Cup", "cup",
+    "Liter", "liter", "l", "Milliliter", "milliliter", "ml", "Kubikmeter", "kubikmeter", "m³",
+    "Kubikcentimeter", "kubikcentimeter","cm³", "Gallon", "gallon", "gal",
+    "Quart", "quart", "qt", "Pint", "pint", "pt", "Cup", "cup",
     
-    "Celsius", "°C", "Fahrenheit", "°F", "Kelvin", "K", "Rankine", "°R"
+    "Celsius", "celsius", "°C", "Fahrenheit", "fahrenhiet", "°F", "Kelvin", "kelvin", "K", "Rankine", "rankine", "°R"
 ]
     output = []
     for i in tokens:
@@ -21,9 +21,24 @@ def hitta_enheter(tokens):
 
 def omvandlare(input):
     lista_att_refrensa = {
-        0.01: ["cm", "cl"],
-        1: ["m", "l", "celsius"],
-        1000: ["kg", "km"],
+        0.000001: ["Kubikcentimeter", "kubikcentimeter", "cm³"],
+        0.001: ["Milliliter", "milliliter", "ml", "Millimeter", "millimeter", "mm", "Milligram", "milligram", "mg"],
+        0.01: ["Centimeter", "centimeter", "cm"],
+        0.24: ["Cup", "cup"],
+        0.568261: ["Pint", "pint", "pt"],
+        1: ["Celsius", "celsius", "°C", "Meter", "meter", "m", "liter", "Liter", "l", "Kubikmeter",
+            "kubikmeter", "m³", "Gram", "gram", "g"],
+        1.101220: ["Quart", "quart", "qt"],
+        2.54: ["Tum", "tum", "in"],
+        3.785: ["Gallon", "gallon", "gal"],
+        30.48: ["Fot", "fot", "ft"],
+        31.1: ["Ounce", "ounce", "oz"],
+        91.44: ["Yard", "yard", "yd"],
+        373.2: ["Pound", "pound", "lb"],
+        1000: ["Kilogram", "kilogram", "kg", "Kilometer", "kilometer", "km"],
+        6350.29318: ["Stone", "stone", "st"],
+        160934.4: ["Mile", "mile", "mi"],
+        1000000: ["Ton", "ton", "t"]
     }
 
     #return find int times ( första enhet/andra enhet)
